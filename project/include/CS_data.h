795faa9f506a6f4e7804139b83135f5cb00ed806
Alexander Nikolaev <aleksandr.n.s@mail.ru>
2018-12-03 03:04:29 +0300

    добавлены зоголовочные файлы классов, в которых содержится (пока что хоть какая-то) информация о серверах, которая нужна в MDS

---
+++ project/include/CS_data.h
@@ -0,0 +1,23 @@
+#ifndef PLYUSHKINCLUSTER_KNOWN_CS_H
+#define PLYUSHKINCLUSTER_KNOWN_CS_H
+
+
+#include <iostream>
+
+typedef struct {
+    std::string addr;
+    uint16_t port = 0;
+} CS_info;
+
+
+class CS_data {
+private:
+    CS_info info;
+public:
+    CS_data(const std::string &addr, uint16_t port) : info{addr, port} {}
+    ~CS_data() = default;
+    CS_info get_info() { return info; }
+};
+
+
+#endif //PLYUSHKINCLUSTER_KNOWN_CS_H
---
+++ project/include/MDS_data.h
@@ -0,0 +1,26 @@
+#ifndef PLYUSHKINCLUSTER_KNOWN_MDS_H
+#define PLYUSHKINCLUSTER_KNOWN_MDS_H
+
+# include <stdint.h>
+
+typedef enum {slave, master} MDS_status;
+
+// все данные об MDS сервере, которые могут понадобиться остальным кускам кода
+typedef struct {
+    // сервер master/slave
+    MDS_status status;
+
+    // ms
+    int64_t timeout;
+} MDS_info;
+
+
+class MDS_data {
+private:
+    MDS_info info;
+public:
+    MDS_info get_info() { return info; }
+};
+
+
+#endif //PLYUSHKINCLUSTER_KNOWN_MDS_H
