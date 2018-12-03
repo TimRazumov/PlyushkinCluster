2cb3fb8a2a812d90d391172ac74d35aeee793aca
Pavel Smirnov <tratata.1950@mail.ru>
2018-12-04 01:32:17 +0300

    1

--- project/src/daemon.cpp
+++ project/src/daemon.cpp
@@ -8,20 +8,20 @@
 
 #include "utils.hpp"
 
-#define THIS_SERVER_PORT 1489
-#define MDS_PORT 1488
+#define THIS_SERVER_PORT 2280
+#define MDS_PORT 5000
 #define MDS_IP "127.0.0.1"
 #define TIMEOUT 1000
 
 
 
 int main() {
-    rpc::server this_srv("0.0.0.0", THIS_SERVER_PORT);
+    rpc::server srv(THIS_SERVER_PORT);
     rpc::client mds(MDS_IP, MDS_PORT);
     mds.set_timeout(TIMEOUT);
 
 
-    this_srv.bind("getattr", 
+    srv.bind("getattr", 
         [&mds](std::string const path) {
             std::string uuid = uuid_from_str(path);
             auto attrs = mds.call("get_attr", uuid).as<std::vector<std::string>>();
@@ -31,8 +31,29 @@ int main() {
             return attri;
         });
     
+    srv.bind("readdir", [&mds](const std::string path) {
+     //   auto ret = mds.call("read_dir", path_uuid).as<>();
+    }); 
+    
+    srv.bind("isDir", [&mds](const std::string path) {
 
-    this_srv.run();
+    }); 
+
+    srv.bind("isFile", [&mds](const std::string path) {
+
+    });
+
+    srv.bind("read", [&mds](const std::string path, size_t size, off_t offset) {
+
+    });
+
+    srv.bind("write", [&mds](const std::string path, const std::string buf,
+                             size_t size, off_t offset) {
+
+    });
+
+
+    srv.run();
 
     return 0;
 }
