580f07c632c5afd9cdb711bccaa5051560241341
Alexander Nikolaev <aleksandr.n.s@mail.ru>
2018-12-05 16:27:36 +0300

    Оганес пошел на хер

--- project/include/CS_utils.h
+++ project/include/CS_utils.h
@@ -1,4 +1,5 @@
 #pragma once
 
+// да, я в курсе, потом нормально будем путь определять
 const char *CS_DIRECTORY = "/home/alexander/plyushkincluster/servers/CS";
 const char *CHUCK_NAME = "123456";
--- project/include/MDS_utils.h
+++ project/include/MDS_utils.h
@@ -3,5 +3,7 @@
 // ms
 const unsigned int default_timeout = 3000;
 
+
+// да, я в курсе, потом нормально будем путь определять
 const char *MDS_DIRECTORY = "/home/alexander/plyushkincluster/servers/MDS";
 const char *test_chunk_UUID = "1e623d52c7";
