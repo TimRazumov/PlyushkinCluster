00b197eb4cf43f070021f881ce7eb1f9f905544d
Oganes Darbinyan <tigon4ik@yandex.ru>
2018-12-06 01:45:35 +0300

    реализовал получение атрибутов, списка файлов, чтение

--- project/src/rpc_daemon.cpp
+++ project/src/rpc_daemon.cpp
@@ -7,29 +7,57 @@
 #include <boost/uuid/name_generator.hpp>
 #include <sys/stat.h>
 
-boost::uuids::uuid fromPath(std::string path) {
-    boost::uuids::nil_generator gen;
-    boost::uuids::name_generator path_gen(gen());
-    return path_gen(path);
-}
+#include "utils.hpp"
 
+const int TIMEOUT = 5000;
+const int LOCAL_PORT = 2280;
+const int MDS_PORT = 5000;
+constexpr size_t CHUNK_SIZE = 2*1024*1024;
+
+
+int min(int a, int b) {
+    return a < b ? a : b;
+}
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
         return -1;
     }
-    rpc::server srv(2280);
+    rpc::server srv(LOCAL_PORT);
 
-    rpc::client clt(argv[1], 5000);
+    rpc::client clt(argv[1], MDS_PORT);
+    
+    clt.set_timeout(TIMEOUT);
 
     srv.bind("getattr", [&clt](const std::string path) {
-        auto path_uuid = to_string(fromPath(path));
-        auto ret = clt.call("get_attr", path_uuid).as<>();
+        auto path_uuid = uuid_from_str(path);
+        auto ret = clt.call("get_attr", path_uuid).as<std::vector<std::string>>();
+        std::vector<int> attrs;
+        for (auto i : ret) {
+            attrs.push_back(std::stoi(i));
+        }
+        return attrs;
     });
 
     srv.bind("readdir", [&clt](const std::string path) {
-        auto path_uuid = to_string(fromPath(path));
-        auto ret = clt.call("read_dir", path_uuid).as<>();
+        auto path_uuid = uuid_from_str(path);
+        int i = 0;
+        auto ret = clt.call("get_chunk", path_uuid, i).as<std::string>();
+        std::vector<std::string> ret_vec;
+        if (ret.size() == 0) {
+            return ret_vec;
+        }
+        do {
+            std::istringstream str(ret);
+            std::string buf;
+            while (std::getline(str, buf)) {
+                ret_vec.push_back(buf);
+            }
+            i++;
+            ret.clear();
+            ret = clt.call("get_chunk", path_uuid, i).as<std::string>();
+        } while (ret.size() != 0);
+        return ret_vec;
     });
     
     srv.bind("isDir", [&clt](const std::string path) {
@@ -37,16 +65,60 @@ int main(int argc, char *argv[]) {
     });
 
     srv.bind("isFile", [&clt](const std::string path) {
-
+        auto path_uuid = uuid_from_str(path);
+        auto ret = clt.call("get_attr", path_uuid).as<std::vector<std::string>>();
+        if (ret.size() != 0 &&
+            std::stoi(ret[0]) >= 0) {
+            return true;
+        }
+        return false;
     });
 
     srv.bind("read", [&clt](const std::string path, size_t size, off_t offset) {
-
+        auto path_uuid = uuid_from_str(path);
+        int chunk_number = offset/CHUNK_SIZE;
+        int local_offset = offset % CHUNK_SIZE;
+        int bias = 0;
+        auto ret = clt.call("get_chunk", path_uuid, chunk_number).as<std::string>();
+        std::string ret_str = "";
+        do {
+            ret_str += ret.substr(local_offset, min(size, CHUNK_SIZE - local_offset));
+            size -= ret_str.size();
+            bias++;
+            local_offset = 0;
+            ret.clear();
+            ret = clt.call("get_chunk", path_uuid,
+                    chunk_number +bias).as<std::string>();
+        } while (size > 0 && ret.size() > 0);
+        return ret_str;
     });
 
     srv.bind("write", [&clt](const std::string path, const std::string buf,
                              size_t size, off_t offset) {
     
+        auto path_uuid = uuid_from_str(path);
+        
     });
+
+    srv.bind("mknod", [&clt](const std::string path) {
+
+    });
+    
+    srv.bind("delete_file", [&clt](const std::string path) {
+
+    });
+
+    srv.bind("rename", [&clt](const std::string from, const std::string to) {
+    
+    });
+    
+    srv.bind("mkdir", [&clt](const std::string path) {
+
+    });
+    
+    srv.bind("rmdir", [&clt](const std::string path) {
+
+    });
+
     srv.run();
 }
--- project/src/utils.cpp
+++ project/src/utils.cpp
@@ -1,30 +1,7 @@
 #include "utils.hpp"
 
-bool isFile(std::string const &path) {
-    struct stat buf;
-    stat(path.c_str(), &buf);
-    if (S_ISREG(buf.st_mode)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-bool isDir(std::string const &path) {
-    struct stat buf;
-    stat(path.c_str(), &buf);
-    if (S_ISDIR(buf.st_mode)) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
 std::string uuid_from_str(std::string const &path) {
-    boost::uuids::string_generator ns;
-    boost::uuids::uuid dns_namespace_uuid 
-        = ns("{6ba7b810-9dad-11d1-80b4-00c04fd430c8}");
-    boost::uuids::name_generator gen(dns_namespace_uuid);
-    boost::uuids::uuid u = gen(path);
-    return to_string(u);
+    boost::uuids::nil_generator gen;
+    boost::uuids::name_generator path_gen(gen());
+    return to_string(path_gen(path));
 }
